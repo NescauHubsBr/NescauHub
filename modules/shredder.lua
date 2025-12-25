3eeea5f549c07528e0e297829fc92a33af8158b8
NescauHubsBr <nescauhubbras@gmail.com>
2025-12-25 15:20:33 -0300

    Update shredder.lua

--- modules/shredder.lua
+++ modules/shredder.lua
@@ -1,1 +1,13 @@
+getgenv().AutoShredder = false
 
+task.spawn(function()
+    while task.wait(3) do
+        if getgenv().AutoShredder then
+            for _,v in pairs(workspace:GetDescendants()) do
+                if v.Name:lower():find("shredder") and v:FindFirstChild("Interact") then
+                    v.Interact:FireServer()
+                end
+            end
+        end
+    end
+end)
