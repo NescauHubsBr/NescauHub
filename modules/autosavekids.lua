b5d1e4c9a39154b896157af416522211fd9a54a2
NescauHubsBr <nescauhubbras@gmail.com>
2025-12-25 15:22:46 -0300

    Update autosavekids.lua

--- modules/autosavekids.lua
+++ modules/autosavekids.lua
@@ -1,1 +1,14 @@
 
+getgenv().AutoSaveKids = false
+
+task.spawn(function()
+    while task.wait(1) do
+        if getgenv().AutoSaveKids then
+            for _,v in pairs(workspace:GetDescendants()) do
+                if v.Name:lower():find("kid") and v:FindFirstChild("Interact") then
+                    v.Interact:FireServer()
+                end
+            end
+        end
+    end
+end)
