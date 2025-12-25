89b20ec872e4a62d9551f802c09536103b536819
NescauHubsBr <nescauhubbras@gmail.com>
2025-12-25 15:24:20 -0300

    Update autoheal.lua

--- modules/autoheal.lua
+++ modules/autoheal.lua
@@ -1,1 +1,15 @@
+local player = game.Players.LocalPlayer
+getgenv().AutoHeal = false
 
+task.spawn(function()
+    while task.wait(1) do
+        if getgenv().AutoHeal then
+            local char = player.Character
+            local hum = char and char:FindFirstChild("Humanoid")
+            if hum and hum.Health < 60 then
+                -- depende do remote de bandage do jogo
+                -- base pronta, ajusta depois
+            end
+        end
+    end
+end)
