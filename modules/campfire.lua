2d42bd10c78c5a0165f2385894b5147dfbda5de1
NescauHubsBr <nescauhubbras@gmail.com>
2025-12-25 15:17:28 -0300

    Update campfire.lua

--- modules/campfire.lua
+++ modules/campfire.lua
@@ -1,1 +1,13 @@
+getgenv().AutoCampfire = false
 
+task.spawn(function()
+    while task.wait(2) do
+        if getgenv().AutoCampfire then
+            for _,v in pairs(workspace:GetDescendants()) do
+                if v.Name:lower():find("campfire") and v:FindFirstChild("Interact") then
+                    v.Interact:FireServer()
+                end
+            end
+        end
+    end
+end)
